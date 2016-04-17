705eab448479056c85fcbf5373496eff00b9753a
Gary Prendergast <gary@divamedia.com>
2016-04-17 21:13:28 +0200

    simplified nag/update removal
    - removed all current update nag codes
    - added new simplified class/function override code for wpmudev, elegant & woo
    - added function to ensure our plugin loads first (needs testing on upgrades as runs on activation, remove nag codes will not work without this)

--- wptoolkit.php
+++ wptoolkit.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Toolkit
  * Plugin URI: https://wptoolkit.com/
  * Description: Premium Theme, Plugin & WooCommerce Extension Manager
- * Version: 1.2.1
+ * Version: 1.2.3
  * Author: WP Toolkit
  * Author URI:  https://wptoolkit.com/ 
  * Copyright: WP Toolkit is based on GPLKit (https://gplkit.com). WP Toolkit is copyright 2016. 
@@ -11,23 +11,39 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
  
- if ( ! defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-/* Turns off WPMUDEV Nags */
-if (!class_exists('WPMUDEV_Update_Notifications')) {
-	class WPMUDEV_Update_Notifications {
-		public function __construct() {}
+//** Make sure this plugin runs first
+function wpt_this_plugin_first() {
+	// ensure path to this file is via main wp plugin path
+	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
+	$active_plugins = get_option('active_plugins');
+	$this_plugin_key = array_search($this_plugin, $active_plugins);
+	if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+		array_splice($active_plugins, $this_plugin_key, 1);
+		array_unshift($active_plugins, $this_plugin);
+		update_option('active_plugins', $active_plugins);
 	}
 }
+add_action("activated_plugin", "wpt_this_plugin_first");
 
-if ( !class_exists('WPMUDEV_Dashboard') ) {
-	class WPMUDEV_Dashboard {
-		public function __construct() {}
-	}
-}	
-/* ENDOF Turns off WPMUDEV Nags */
+//** Turns off WPMUDEV Dashboard Nags */
+if ( ! class_exists('WPMUDEV_Dashboard_Notice3') ) {
+	class WPMUDEV_Dashboard_Notice3 {}
+}
+
+//** Turn Off Elegant Themes updates class.
+if ( ! class_exists( 'ET_Core_Updates' ) ) {
+   	class ET_Core_Updates {}
+}
+
+//** Turn Off Woo Updater Nags
+if ( ! function_exists( 'woothemes_updater_notice' ) ) {
+	function woothemes_updater_notice() {}
+}
 	
 if ( ! class_exists( 'WPToolKit' ) ) {
 
@@ -107,23 +123,40 @@ function GK() {
 // Global for backwards compatibility.
 $GLOBALS['wptoolkit'] = GK();
 
+function WPT_remote_download($url, $save_path = false){
+	// Use wp_remote_get to fetch the data
+	$response = wp_remote_get($url, array("timeout" => PHP_INT_MAX));
 
-//** Overrides actions set by Elegant Themes that prevents plugin and theme updates.
-function WPT_override_et() {
-	if(isset($GLOBALS['et_core_updates'])){
-		// lets override Elegant theme's
-		remove_action( 'pre_set_site_transient_update_plugins', array( $GLOBALS['et_core_updates'], 'check_plugins_updates' ));
-		remove_action( 'site_transient_update_plugins', array( $GLOBALS['et_core_updates'], 'add_plugins_to_update_notification' ));
-		remove_action( 'pre_set_site_transient_update_themes', array( $GLOBALS['et_core_updates'], 'check_themes_updates' ));
-		remove_action( 'site_transient_update_themes', array( $GLOBALS['et_core_updates'], 'add_themes_to_update_notification' ));
-	}	
+	// Save the body part to a variable
+	$zip = $response['body'];
+
+	
+	// In the header info is the name of the XML or CVS file. I used preg_match to find it
+	preg_match("/filename\s*=\s*(\\\"[^\\\"]*\\\"|'[^']*)/i", $response['headers']['content-disposition'], $match);
+
+	if($save_path){
+		// Create the name of the file and the declare the directory and path
+		$file = trailingslashit($save_path).$match[1];
+
+		// Now use the standard PHP file functions
+		$fp = fopen($file, "w");
+		fwrite($fp, $zip);
+		fclose($fp);
+		return true;
+	}else{
+		if($zip){
+			return array("filenam" => $match[1], "body" => $zip);
+		}else{ 
+			return false;
+		}
+	}
 }
-add_action( 'admin_init', 'WPT_override_et',PHP_INT_MAX );
 
-//** This is used to force WPToolkit to update its lists of plugins and themes
+//** Force WPToolkit to update its lists of plugins and themes
 function WPT_force_update_lists(){
 	WPToolKit_Updates::get_plugin_catalogue();
 	die();
 }
+
 add_action( 'wp_ajax_get_plugin_catalogue', "WPT_force_update_lists" );
 add_action( 'wp_ajax_nopriv_get_plugin_catalogue', "WPT_force_update_lists");
